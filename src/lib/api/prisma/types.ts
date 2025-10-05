97c2ab7e3eb5d54bb8f4fa9c47d0fe32c9ad61a7
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2025-10-05 23:23:05 +0200

    feat: migrate to prisma-client-js generator provider

--- src/lib/api/prisma/types.ts
+++ src/lib/api/prisma/types.ts
@@ -1,8 +1,6 @@
 // re-export all client **types** only
 export type * from "./generated/client"
 
-export type * from "@prisma/client/runtime/library"
-
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - re-export so we still get actual enums
 export * from "./generated/enums"
