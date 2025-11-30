123edca21fd9ec09ad6e1e235b2ceefd1426bb7a
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2025-11-30 01:05:00 +0100

    feat: template frontend overhaul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link"
-
 import { LatestPost } from "~/components/post"
 import { api, getQueryClient } from "~/lib/trpc/server"
 import { HydrateClient, prefetch } from "~/lib/trpc/utils/prefetch"
@@ -15,42 +13,35 @@ export default async function Home() {
 
 	return (
 		<HydrateClient>
-			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+			<main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-gray-950 to-gray-900 text-gray-200">
 				<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-					<h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-						Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-					</h1>
-					<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-						<Link
-							className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-							href="https://create.t3.gg/en/usage/first-steps"
-							target="_blank"
-						>
-							<h3 className="text-2xl font-bold">First Steps →</h3>
-							<div className="text-lg">
-								Just the basics - Everything you need to know to set up your
-								database and authentication.
-							</div>
-						</Link>
-						<Link
-							className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-							href="https://create.t3.gg/en/introduction"
-							target="_blank"
-						>
-							<h3 className="text-2xl font-bold">Documentation →</h3>
-							<div className="text-lg">
-								Learn more about Create T3 App, the libraries it uses, and how
-								to deploy it.
-							</div>
-						</Link>
-					</div>
-					<div className="flex flex-col items-center gap-2">
-						<p className="text-2xl text-white">
-							{hello ? hello.greeting : "Loading tRPC query..."}
+					<div className="text-center space-y-2">
+						<h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+							Create{" "}
+							<span className="text-[hsl(280,100%,70%)] line-through">T3</span>{" "}
+							<span className="bg-clip-text text-transparent bg-linear-to-br from-cyan-600 to-cyan-700">
+								Mark's
+							</span>{" "}
+							App
+						</h1>
+
+						<p className="text-lg">
+							Next.js + Tailwind CSS + tRPC + Drizzle ORM
 						</p>
 					</div>
 
-					<LatestPost />
+					<div className="w-full max-w-md rounded-lg flex flex-col gap-2 p-4 bg-gray-900 shadow-xl border border-gray-800">
+						<h2 className="text-2xl font-bold">TRPC tests</h2>
+						<h3 className="text-lg font-medium">Query</h3>
+
+						<p className="text-2xl text-white text-center">
+							{hello ? hello.greeting : "Loading..."}
+						</p>
+
+						<h3 className="text-lg font-medium">Mutation + query</h3>
+
+						<LatestPost />
+					</div>
 				</div>
 			</main>
 		</HydrateClient>
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -19,12 +19,13 @@ export function LatestPost() {
 	)
 
 	return (
-		<div className="w-full max-w-xs">
+		<div className="w-full">
 			{latestPost ? (
 				<p className="truncate">Your most recent post: {latestPost.name}</p>
 			) : (
 				<p>You have no posts yet.</p>
 			)}
+
 			<form
 				onSubmit={e => {
 					e.preventDefault()
@@ -39,6 +40,7 @@ export function LatestPost() {
 					onChange={e => setName(e.target.value)}
 					className="w-full rounded-full bg-white/10 px-4 py-2 text-white"
 				/>
+
 				<button
 					type="submit"
 					className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
